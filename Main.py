568217bbd5cb762679d933448808d73eebd0362a
EMIL EMILOV DIMITROV <emil.emilov@alu.uclm.es>
2021-06-15 17:57:48 +0200

    Test Ai versus Ai...

--- Main.py
+++ Main.py
@@ -3,73 +3,41 @@ import Utils
 from MinMax import MiniMax
 import os
 if __name__ == '__main__':
-    welcomeS = "MINIMAX TEST MENU OPTIONS" \
-               "\n1-Human vs algo" \
-               "\n5-Exit sim" \
-               "\n--------------------------"
-    mainMenu = {
-        1: "Human vs algo",
-        2: "EXIT"
-    }
-    def getFloat(start, end):
-        ok = False
-        num = 0
-        while not ok:
-            try:
-                num = float(input(f"Insert probability betweeen {start} and {end}\n"))
-                if num in [x * .1 for x in range(11)]:
-                    ok = True
-            except ValueError:
-                print(f"Remember, only floats betweeen {start} and {end}!")
-        return num
 
 
-    def getInteger(warning):
-        ok = False
-        num = 0
-        while not ok:
-            try:
-                num = int(input("Insert option\n"))
-                ok = True
-            except ValueError:
-                print(f"Remember, {warning}!")
-        return num
-
-    def getStatePredefined():
-        state = Utils.getChessInstancePosition(0, 3, 1)
+    def getStatePredefined(prob, seed, turn):
+        state = Utils.getChessInstancePosition(prob, seed, turn)
         state.m_board[2][7] = 10
         state.m_board[4][7] = 10
 
         return state
 
-
-    def menu(options, header):
-        exited= False
-        ext_str="EXIT"
-        while not exited:
-            print(header)
-            opt = getInteger('only integers')
-            if opt in options:
-                if options[opt] == ext_str:
-                    exited = True
-                else:
-                    print(f"{options[opt]} selected\n")
-                    return opt
-            else:
-                print(f"Valid choices: {options}")
-        print("END")
-    ####################WORK FROM THIS POINT##########################
-    #optIn = menu(mainMenu, welcomeS)
-    optIn = 1
-    if optIn==1:
-        #prob = getFloat(0,1)
-        #print(f"Selected probability is {prob}\n")
-        #st = Utils.getChessInstancePosition(prob, 100, 0)
-        st = getStatePredefined()
-        print(st.m_board)
+    def AIvsAI(maxMoves, seed, turn, prob):
+        st = getStatePredefined(prob, seed, turn)
         st.reloadPositions()
+        print(f"INITIAL")
         Utils.printBoard(st)
-        v, m = (MiniMax(st, st.turn))
-        print(f"Evaluation value is {v}")
-        print(f"Action is ${m}")
+        print("--------------------------------\n\n\n")
+        final = False
+        while maxMoves >0 or not final:
+            v, m = (MiniMax(st, turn))
+            print(f"Evaluation value is {v}")
+            print(f"Action is ${m}")
+            st = st.applyAction(m)
+            Utils.printBoard(st)
+            print(f"--------------{maxMoves}---------------\n\n\n")
+            final = st.isFinal
+            turn = (turn+1)%2
+            maxMoves = maxMoves - 1
+    AIvsAI(5, 3, 1, 0)
+
+    '''
+    st = getStatePredefined()
+    print(st.m_board)
+    st.reloadPositions()
+    Utils.printBoard(st)
+    v, m = (MiniMax(st, st.turn))
+    print(f"Evaluation value is {v}")
+    print(f"Action is ${m}")
+    '''
 
