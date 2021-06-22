5f9866402a2ec9ceb302eca5c214858d84c3e492
EMIL EMILOV DIMITROV <emil.emilov@alu.uclm.es>
2021-06-22 18:01:02 +0200

    Unify methods MiniMax and AlfaBeta.

--- Main.py
+++ Main.py
@@ -1,151 +1,128 @@
-# main to test the methods
-import sys
-
 import Utils
-from AlfaBeta import alfa_beta
-from MinMax import MiniMax
+from MiniMax import minimax
 from Position import Position
 
-if __name__ == '__main__':
-    '''
-    method = str(sys.argv[1])
-    inicio = bool(sys.argv[2])
-    profundidad = int(sys.argv[3])
-    color = str(sys.argv[4])
-    maxJugadas = int(sys.argv[5])
-    prob = float(sys.argv[6])
-    seed = int(sys.argv[7])
-    '''
+
+def getStatePredefined(prob, seed, turn):
+    state = Utils.get_chess_instance_position(prob, seed, turn)
+    return state
 
 
+def AIvsAI(maxMoves, seed, turn, prob, initial, prune=False):
+    if (initial):
+        st = Utils.get_chess_instance(prob, seed, turn)
+    else:
+        st = getStatePredefined(prob, seed, turn)
+    st.reloadPositions()
+    print(f"INITIAL")
+    Utils.print_board(st)
+    print("--------------------------------\n\n\n")
+    final = False
+    while maxMoves > 0 and not final:
+        v, m, gen, exp = (minimax(st, turn, pruning=prune))
+        print(f"Turn is {turn}")
+        print(f"Evaluation value is {v}")
+        print(f"Action is ${m}")
+        if (st.m_board[m.m_finalPos.row][m.m_finalPos.col] == 5 or st.m_board[m.m_finalPos.row][
+            m.m_finalPos.col] == 11):
+            final = True
+        st = st.applyAction(m)
+        st.depth = 3
+        Utils.print_board(st)
+        print(f"--------------{maxMoves}---------------\n\n\n")
+        turn = (turn + 1) % 2
+        maxMoves = maxMoves - 1
 
-    def getStatePredefined(prob, seed, turn):
-        state = Utils.get_chess_instance_position(prob, seed, turn)
+    if (v > 0):
+        print("Ganan Blancas")
+    elif (v < 0):
+        print("Ganan Negras")
+    else:
+        print("Tablas")
+    print(f"Generados: {gen}")
+    print(f"Expandidos: {exp}")
+
+
+def makeMovement(state, turn):
+    if turn:
+        i = 1
+        for pos in state.wElemList:
+            valuePiece = state.m_board[pos[0]][pos[1]]
+            print(str(i) + ": Mover " + str(Utils.valueNames[valuePiece]) + " en [" + str(pos[0]) + "," + str(
+                pos[1]) + "]")
+            i = i + 1
+
+        opt = int(input("Selecciona una pieza: "))
+        posPiezaElegida = state.wElemList[opt - 1]
+        state.m_agentPos = Position(posPiezaElegida[0], posPiezaElegida[1])
+        pieza = Utils.piece_factory(state.m_board[posPiezaElegida[0]][posPiezaElegida[1]])
+        print(pieza.get_possible_actions(state))
+        posibleActions = (pieza.get_possible_actions(state))
+        i = 1
+        for action in posibleActions:
+            print(str(i) + ": Mover a " + str(action.m_finalPos))
+            i = i + 1
+        act = int(input("Selecciona una accion: "))
+        state = state.applyAction(posibleActions[act - 1])
+        state.depth = 3
+        Utils.print_board(state)
         return state
 
-    def AIvsAI(maxMoves, seed, turn, prob, initial):
-        if (initial):
-            st = Utils.get_chess_instance(prob, seed, turn)
-        else:
-            st = getStatePredefined(prob, seed, turn)
-        st.reloadPositions()
-        print(f"INITIAL")
-        Utils.print_board(st)
-        print("--------------------------------\n\n\n")
-        final = False
-        while maxMoves >0 and not final:
-            v, m, gen, exp = (MiniMax(st, turn))
-            print(f"Turn is {turn}")
-            print(f"Evaluation value is {v}")
-            print(f"Action is ${m}")
-            if (st.m_board[m.m_finalPos.row][m.m_finalPos.col] == 5  or st.m_board[m.m_finalPos.row][m.m_finalPos.col] == 11):
-                final = True
-            st = st.applyAction(m)
-            st.depth = 3
-            Utils.print_board(st)
-            print(f"--------------{maxMoves}---------------\n\n\n")
-            turn = (turn+1)%2
-            maxMoves = maxMoves - 1
-
-        if (v > 0):
-            print("Ganan Blancas")
-        elif (v < 0):
-            print("Ganan Negras")
-        else:
-            print("Tablas")
-        print(f"Generados: {gen}")
-        print(f"Expandidos: {exp}")
-
-    def makeMovement(state, turn):
-        if turn:
-            i = 1
-            for pos in state.wElemList:
-                valuePiece = state.m_board[pos[0]][pos[1]]
-                print(str(i)+ ": Mover "+str(Utils.valueNames[valuePiece])+" en ["+str(pos[0]) + "," + str(pos[1])+"]"  )
-                i = i+1
-
-            opt = int(input("Selecciona una pieza: "))
-            posPiezaElegida = state.wElemList[opt - 1]
-            state.m_agentPos = Position(posPiezaElegida[0], posPiezaElegida[1])
-            pieza = Utils.piece_factory(state.m_board[posPiezaElegida[0]][posPiezaElegida[1]])
-            print(pieza.get_possible_actions(state))
-            posibleActions = (pieza.get_possible_actions(state))
-            i = 1
-            for action in posibleActions:
-                print(str(i) + ": Mover a " +str(action.m_finalPos) )
-                i = i + 1
-            act = int(input("Selecciona una accion: "))
-            state = state.applyAction(posibleActions[act-1])
-            state.depth = 3
-            Utils.print_board(state)
-            return state
-
-        else:
-            i = 1
-            for pos in state.bElemList:
-                valuePiece = state.m_board[pos[0]][pos[1]]
-                print(str(i)+ ": Mover "+str(Utils.valueNames[valuePiece])+" en ["+str(pos[0]) + "," + str(pos[1])+"]"  )
-                i = i+1
-            opt = int(input("Selecciona una pieza: "))
-            posPiezaElegida = state.bElemList[opt - 1]
-            state.m_agentPos = Position(posPiezaElegida[0], posPiezaElegida[1])
-            pieza = Utils.piece_factory(state.m_board[posPiezaElegida[0]][posPiezaElegida[1]])
-            posibleActions = (pieza.get_possible_actions(state))
-            i = 1
-            for action in posibleActions:
-                print(str(i) + ": Mover a " +str(action.m_finalPos) )
-                i = i + 1
-            act = int(input("Selecciona una accion: "))
-            state = state.applyAction(posibleActions[act-1])
-            state.depth = 3
-            Utils.print_board(state)
-            return state
-            a = 0
+    else:
+        i = 1
+        for pos in state.bElemList:
+            valuePiece = state.m_board[pos[0]][pos[1]]
+            print(str(i) + ": Mover " + str(Utils.valueNames[valuePiece]) + " en [" + str(pos[0]) + "," + str(
+                pos[1]) + "]")
+            i = i + 1
+        opt = int(input("Selecciona una pieza: "))
+        posPiezaElegida = state.bElemList[opt - 1]
+        state.m_agentPos = Position(posPiezaElegida[0], posPiezaElegida[1])
+        pieza = Utils.piece_factory(state.m_board[posPiezaElegida[0]][posPiezaElegida[1]])
+        posibleActions = (pieza.get_possible_actions(state))
+        i = 1
+        for action in posibleActions:
+            print(str(i) + ": Mover a " + str(action.m_finalPos))
+            i = i + 1
+        act = int(input("Selecciona una accion: "))
+        state = state.applyAction(posibleActions[act - 1])
+        state.depth = 3
+        Utils.print_board(state)
+        return state
+        a = 0
 
 
-    def humanvsAI(seed, turn, prob, initial):
-        if (initial):
-            st = Utils.get_chess_instance(prob, seed, turn)
-        else:
-            st = getStatePredefined(prob, seed, turn)
-        st.reloadPositions()
-        print(f"INITIAL")
-        Utils.print_board(st)
-        print("--------------------------------\n\n\n")
-        final = False
-        while not final:
-            #turno jugador
-            st = makeMovement(st, turn)
-            if (st.isFinal):
-                print("Humano gana")
-                break
-            #turno agente
-            v, m, gen, exp = (MiniMax(st, turn))
-            print(f"Evaluation value is {v}")
-            print(f"Action is ${m}")
-            if (st.m_board[m.m_finalPos.row][m.m_finalPos.col] == 5  or st.m_board[m.m_finalPos.row][m.m_finalPos.col] == 11):
-                final = True
-            st = st.applyAction(m)
-            st.depth = 3
-            Utils.print_board(st)
-            print(f"-----------------------------\n\n\n")
-        print(f"Generados: {gen}")
-        print(f"Expandidos: {exp}")
-
-
-
-    #humanvsAI(927, 0, 0.1, False)
-    AIvsAI(100, 123, 0, 0.2, False)
-
-
-
-    '''
-    st = getStatePredefined()
-    print(st.m_board)
+def humanvsAI(seed, turn, prob, initial, prune=False):
+    if (initial):
+        st = Utils.get_chess_instance(prob, seed, turn)
+    else:
+        st = getStatePredefined(prob, seed, turn)
     st.reloadPositions()
+    print(f"INITIAL")
     Utils.print_board(st)
-    v, m = (MiniMax(st, st.turn))
-    print(f"Evaluation value is {v}")
-    print(f"Action is ${m}")
-    '''
+    print("--------------------------------\n\n\n")
+    final = False
+    while not final:
+        # turno jugador
+        st = makeMovement(st, turn)
+        if (st.isFinal):
+            print("Humano gana")
+            break
+        # turno agente
+        v, m, gen, exp = (minimax(st, turn, pruning=prune))
+        print(f"Evaluation value is {v}")
+        print(f"Action is ${m}")
+        if (st.m_board[m.m_finalPos.row][m.m_finalPos.col] == 5 or st.m_board[m.m_finalPos.row][
+            m.m_finalPos.col] == 11):
+            final = True
+        st = st.applyAction(m)
+        st.depth = 3
+        Utils.print_board(st)
+        print(f"-----------------------------\n\n\n")
+    print(f"Generados: {gen}")
+    print(f"Expandidos: {exp}")
 
+
+if __name__ == '__main__':
+    # humanvsAI(927, 0, 0.1, False)
+    AIvsAI(100, 123, 0, 0.2, False, True)
--- MiniMax.py
+++ MiniMax.py
@@ -1,16 +1,5 @@
 import copy
-import sys
 import Utils
-from State import State
-import Piece
-import math as m
-from Piece import Piece
-from Rook import Rook
-from Knight import Knight
-from Pawn import Pawn
-from King import King
-from Bishop import Bishop
-from Queen import Queen
 from Position import Position
 
 MINUS_INF = -1000000
@@ -48,7 +37,7 @@ def successors(state, turn):
 def get_states(x, y, state):
     state_list = []
     value = state.m_board[x][y]
-    piece = piece_factory(value)
+    piece = Utils.piece_factory(value)
     # make sure it is a different state
     mod_state = copy.deepcopy(state)
     mod_state.m_agentPos = Position(x, y)
@@ -58,75 +47,48 @@ def get_states(x, y, state):
     return state_list
 
 
-def piece_factory(value):
-    if value == Utils.wPawn:
-        return Pawn(0)
-    elif value == Utils.bPawn:
-        return Pawn(1)
-    elif value == Utils.wRook:
-        return Rook(0)
-    elif value == Utils.bRook:
-        return Rook(1)
-    elif value == Utils.wKing:
-        return King(0)
-    elif value == Utils.bKing:
-        return King(1)
-    elif value == Utils.wQueen:
-        return Queen(0)
-    elif value == Utils.bQueen:
-        return Queen(1)
-    elif value == Utils.wBishop:
-        return Bishop(0)
-    elif value == Utils.bBishop:
-        return Bishop(1)
-    elif value == Utils.wKnight:
-        return Knight(0)
-    elif value == Utils.bKnight:
-        return Knight(1)
-    else:
-        return None
-
-
-def alfa_beta(state, turn):
+def minimax(state, turn, pruning):
     if turn:
-        v, m = minvalue(state, turn, None, MINUS_INF, PLUS_INF)
+        v, m = minvalue(state, turn, None, pruning=pruning)
         return v, m, generated, expanded
     else:
-        v, m = maxvalue(state, turn, None, MINUS_INF, PLUS_INF)
+        v, m = maxvalue(state, turn, None, pruning=pruning)
         return v, m, generated, expanded
 
 
-def minvalue(state, turn, m, alfa, beta):
+def minvalue(state, turn, m, alfa=MINUS_INF, beta=PLUS_INF, pruning=False):
     turn = (turn + 1) % 2
     if state.isFinal or state.depth == 0:
         m = state.move
         return state.getEval(), m
     v = PLUS_INF
     for st in successors(state, turn):
-        # min(v, minimax alfa_beta)
-        act_v = maxvalue(st, turn, m, alfa, beta)[0]
+        # min(v, minimax minimax)
+        act_v = maxvalue(st, turn, m, alfa, beta, pruning)[0]
         if act_v < v:
             v = act_v
             m = st.move
-        if v <= alfa:
-            return v, m
-        beta = min(v, beta)
+        if pruning:
+            if v <= alfa:
+                return v, m
+            beta = min(v, beta)
     return v, m
 
 
-def maxvalue(state, turn, m, alfa, beta):
+def maxvalue(state, turn, m, alfa=MINUS_INF, beta=PLUS_INF, pruning=False):
     turn = (turn + 1) % 2
     if state.isFinal or state.depth == 0:
         m = state.move
         return state.getEval(), m
     v = MINUS_INF
     for st in successors(state, turn):
-        # max(v, minimax alfa_beta)
-        act_v = minvalue(st, turn, m, alfa, beta)[0]
+        # max(v, minimax minimax)
+        act_v = minvalue(st, turn, m, alfa, beta, pruning)[0]
         if act_v > v:
             v = act_v
             m = st.move
-        if v >= beta:
-            return v, m
-        alfa = max(v, alfa)
+        if pruning:
+            if v >= beta:
+                return v, m
+            alfa = max(v, alfa)
     return v, m
